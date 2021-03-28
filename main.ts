12fd07a1795d3457be0527d06c9edcf18c7b5245
bryan weinstein <bryanweinstein@outlook.com>
2021-03-28 16:08:38 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let choice = 0
 // Happy Face and all light up!
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
@@ -5,8 +6,23 @@ input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 1)
 })
+input.onButtonPressed(Button.AB, function () {
+    choice = randint(0, 3)
+    if (choice == 0) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else if (choice == 1) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else if (choice == 2) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P3, 1)
+    }
+})
 // Sad Face and none-light up
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
     pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P3, 0)
 })
