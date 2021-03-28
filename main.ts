a5660b1464cde0d7a5a6d1be30d6e02235733401
bryan weinstein <bryanweinstein@outlook.com>
2021-03-28 16:17:15 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,15 +7,13 @@ input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P2, 1)
 })
 input.onButtonPressed(Button.AB, function () {
-    choice = randint(0, 3)
+    choice = randint(0, 2)
     if (choice == 0) {
         pins.digitalWritePin(DigitalPin.P0, 1)
     } else if (choice == 1) {
         pins.digitalWritePin(DigitalPin.P1, 1)
-    } else if (choice == 2) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
     } else {
-        pins.digitalWritePin(DigitalPin.P3, 1)
+        pins.digitalWritePin(DigitalPin.P2, 1)
     }
 })
 // Sad Face and none-light up
@@ -24,5 +22,4 @@ input.onButtonPressed(Button.B, function () {
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P3, 0)
 })
